63072f6b836c3ae1d33d48bb0d0dfe211889411e
Ericson <ericsonbsantos7@hotmail.com>
2023-08-14 10:05:37 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // subistituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter imgaem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  //
+}
